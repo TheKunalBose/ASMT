e9b4cded304ac186be8d0ce6ec79feed8e674661
kunal <kunalvon3002@gmail.com>
2024-05-25 09:39:42 +0530

    ASMT full version

---
+++ Data/front_test1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Asset Management System TruKKer", page_icon=":tada:", layout = "wide")
+
+st.subheader("Hello welcome to TruKKer's Asset Management System :wave:")
+st.title("Complete TruKKers dataset")
+st.write("") 
